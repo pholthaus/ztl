91acb6c9e2bf03a09c63cbee842701f2ffff7a2b
Patrick Holthaus <p.holthaus@herts.ac.uk>
2023-05-23 17:45:17 +0100

    use more logging

--- src/ztl/core/task.py
+++ src/ztl/core/task.py
@@ -1,5 +1,7 @@
-import logging
 import time
+import logging
+logging.basicConfig(level=logging.INFO)
+
 
 from threading import Thread
 from ztl.core.protocol import State
@@ -42,6 +44,7 @@ class TaskExecutor(Thread):
 
   def __init__(self, cls, *parameters):
     Thread.__init__(self)
+    self.logger = logging.getLogger(type(cls).__name__)
     self.cls = cls
     self.parameters = parameters
     self.task = None
@@ -51,41 +54,41 @@ class TaskExecutor(Thread):
 
 
   def run(self):
-    print("Initiating task...")
+    self.logger.debug("Initiating task with parameters '%s'...", self.parameters)
     self.task = self.cls(*self.parameters)
     success = self.task.initialise()
     if success:
       if not self._prevent:
-        print("Accepting and executing task...")
+        self.logger.debug("Accepting and executing task...")
         self._state = State.ACCEPTED
         success = self.task.execute()
         if success:
-          print("Task completed successfully.")
+          self.logger.debug("Task execution completed successfully.")
           self._state = State.COMPLETED
         else:
-          print("Task failed.")
+          self.logger.debug("Task execution failed.")
           self._state = State.FAILED
       else:
         self._state = State.FAILED
         logging.warn("Task execution prevented during initialising.")
     else:
-      print("Rejecting task...")
+      self.logger.debug("Task initialising failed, rejecting task.")
       self._state = State.REJECTED
 
 
   def stop(self):
     if self.task is None:
-      print("Preventing task from executing...")
+      self.logger.debug("Preventing task from executing...")
       self._prevent = True
       self._state = State.ABORTED
     else:
-      print("Aborting task execution...")
+      self.logger.debug("Aborting task execution...")
       success = self.task.abort()
       if success:
         print("Task aborted successfully.")
         self._state = State.ABORTED
       else:
-        logging.warning("Task could not be aborted.")
+        self.logger.debug("Task could not be aborted.")
     return self._state
 
 
