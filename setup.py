c1f1a893b2533bca20093344edbc577cb9119970
Patrick Holthaus <patrick.holthaus@googlemail.com>
2025-09-18 16:42:04 +0100

    version bump

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='ztl',
-      version='0.0.2',
+      version='0.0.3',
       description='A thin library relying on zmq to dispatch tasks',
       author='Patrick Holthaus',
       author_email='patrick.holthaus@googlemail.com',
