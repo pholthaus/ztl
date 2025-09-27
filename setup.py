59507effd430c9f51633b7f69d5714f65c23add1
Patrick Holthaus <patrick.holthaus@googlemail.com>
2025-09-27 23:30:37 +0100

    try entry_points

--- setup.py
+++ setup.py
@@ -12,6 +12,17 @@ setup(name='ztl',
       packages=['ztl', 'ztl.core', 'ztl.example', 'ztl.script'],
       scripts=['src/ztl/example/simple_client.py',
             'src/ztl/example/simple_server.py',
+            'src/ztl/example/task_client.py',
+            'src/ztl/example/task_server.py',
             'src/ztl/script/run_script.py'
-      ]
+      ],
+      entry_points={
+            'console_scripts': [
+                  'ztl_simple_client=ztl.example.simple_client:main_cli',
+                  'ztl_simple_server=ztl.example.simple_server:main_cli',
+                  'ztl_task_server=ztl.example.task_server:main_cli',
+                  'ztl_task_client=ztl.example.task_client:main_cli',
+                  'ztl_run_script=ztl.script.run_script:main_cli'
+            ]
+      }
 )
