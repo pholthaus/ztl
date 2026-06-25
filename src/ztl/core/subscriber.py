49d10b9840f546e141c45ffc8444caf9c3ef149f
Patrick Holthaus <patrick.holthaus@googlemail.com>
2026-06-25 14:52:18 +0100

    implement subscriber as class

--- src/ztl/core/subscriber.py
+++ src/ztl/core/subscriber.py
@@ -1,11 +1,14 @@
 import zmq
 import logging
 
+from threading import Thread
+
 logging.basicConfig(level=logging.INFO)
 
-class ObjectSubscriber(object):
+class ObjectSubscriber(Thread):
   
   def __init__(self, host, port, scope):
+    Thread.__init__(self)
     self.logger = logging.getLogger('object-subscriber')
     context = zmq.Context()
     self.socket = context.socket(zmq.SUB)
@@ -13,19 +16,24 @@ class ObjectSubscriber(object):
     self.socket.connect(address)
     self.socket.setsockopt_string(zmq.SUBSCRIBE, scope)
     self.logger.info("Subscriber '%s' established at '%s'" % (scope, address))
-
+    self.active = False
     
-  def listen(self):
+  def run(self):
     self.logger.info("Start listening...")
-    while True:
+    self.active = True
+    try:
+      while self.active:
         topic = self.socket.recv_string()
         obj = self.socket.recv_pyobj()
-        print(obj)
-
-def main_cli():
-  p = ObjectSubscriber("localhost", 5555, "camera_frame")
-  p.listen()
-
-if __name__ == "__main__":
-
-  main_cli()
+        self.callback(obj)
+    except Exception as e:
+      self.logger.error("Listening failed: '%s'", e)
+      
+    self.active = False
+    self.logger.info("Finished listening.")
+      
+  def stop(self):
+    self.active = False
+    
+  def callback(self, obj):
+    raise RuntimeError("Function 'callback' not implemented in subscriber.")
